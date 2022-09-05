e2708122e12b29b629cba2bc1411cf760ea01029
Julipe117 <110401271+Julipe117@users.noreply.github.com>
2022-09-05 12:19:07 +0200

    Update EJERCICIO 05.js

--- EJERCICIO 05.js
+++ EJERCICIO 05.js
@@ -4,11 +4,11 @@ let altura_m = 1.79;
 
 let peso_kg = 78.793;
 
-let altura_m_aprox = altura_m.toFixed(1);
+let altura_m_aprox = Math.ceil(altura_m);
 console.log(altura_m_aprox);
 
-let peso_kg_aprox = peso_kg.toPrecision(4);
+let peso_kg_aprox = Math.floor(peso_kg);
 console.log(peso_kg_aprox);
 
-let max_valor_JS = Number.MAX_VALUE + 1;
+let max_valor_JS = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
 console.log(max_valor_JS);
