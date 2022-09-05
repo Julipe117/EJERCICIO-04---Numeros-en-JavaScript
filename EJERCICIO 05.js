4a653dfbeb354326d952a9c6ad02d460e377cfc5
Julipe117 <110401271+Julipe117@users.noreply.github.com>
2022-09-05 12:13:08 +0200

    Add files via upload

---
+++ EJERCICIO 05.js
@@ -0,0 +1,14 @@
+let altura_cm = 179;
+
+let altura_m = 1.79;
+
+let peso_kg = 78.793;
+
+let altura_m_aprox = altura_m.toFixed(1);
+console.log(altura_m_aprox);
+
+let peso_kg_aprox = peso_kg.toPrecision(4);
+console.log(peso_kg_aprox);
+
+let max_valor_JS = Number.MAX_VALUE + 1;
+console.log(max_valor_JS);
